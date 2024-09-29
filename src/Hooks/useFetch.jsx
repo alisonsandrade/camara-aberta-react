6dfc5b89233c37904c785a7be0719202818e2d6c
AlisonSandrade <alison.sandrade@hotmail.com>
2024-09-29 06:08:53 -0300

    feat: hook fetch

---
+++ src/Hooks/useFetch.jsx
@@ -0,0 +1,44 @@
+import React, { useCallback } from "react";
+
+const useFetch = () => {
+  const [data, setData] = React.useState(null);
+  const [loading, setLoading] = React.useState(null);
+  const [error, setError] = React.useState(false);
+
+  const request = useCallback(async (url, options) => {
+    try {
+      setError(null);
+      setLoading(true);
+
+      const response = await fetch(url, options);
+      const json = await response.json();
+
+      if (!response.ok)
+        throw new Error(
+          `${json.status} - ${json.detail}` ||
+            response.statusText ||
+            "Erro desconhecido"
+        );
+
+      setData(json);
+
+      return { response, data };
+    } catch (error) {
+      console.log("Error useFetch: ", error);
+      console.log(error.message);
+
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  return {
+    data,
+    loading,
+    error,
+    request,
+  };
+};
+
+export default useFetch;
