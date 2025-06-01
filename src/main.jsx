7bbfacb214c6bd35068a66bebc98e2e3dfeb53c1
Alison Andrade <alison.sandrade@hotmail.com>
2025-06-01 13:02:05 -0300

    build: indicação da basename ao createBrownserRouter a fim de reconhecer o caminho correto da raiz

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,9 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import routes from "./Routes/index.jsx";
 
-const router = createBrowserRouter(routes);
+const router = createBrowserRouter(routes, {
+  basename: "/camara-aberta-react"
+});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
